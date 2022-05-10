9d29b9d222489b98514b65f395a438325b5c3542
vannt@DESKTOP-NJIFU70 <vannt95@fpt.com.vn>
2022-05-10 10:21:49 +0700

    06-Setup_Client-P2

--- client/src/PageRender.tsx
+++ client/src/PageRender.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { IParams } from './utils/TypeScript'
+import NotFound from './components/global/NotFound'
+
+
+const generatePage = (name: string) => {
+  const component = () => require(`./pages/${name}`).default
+
+  try {
+    return React.createElement(component())
+  } catch (err) {
+    return <NotFound />;
+  }
+}
 
 const PageRender = () => {
-  const params = useParams()
+  const { page, slug } = useParams()
 
-  console.log(params)
-  return (
-    <div>
-      
-    </div>
-  )
+  let name = ''
+
+  if(page){
+    name = slug ? `${page}/[slug]` : `${page}`
+  }
+
+  return generatePage(name)
 }
 
 export default PageRender
